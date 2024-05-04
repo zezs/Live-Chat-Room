2a2b60a3d8f087612e67a1a88478d72867eeac3b
zezs <my.version.ctrl@gmail.com>
2024-05-04 20:37:05 +0200

    starting flask server and creating frontend forms

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request, session, redirect
-from flask_socketio import join_room, leave_room, sned, SocketIO
+from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Qwerty16"
 socketio = SocketIO(app)
+
+rooms = {}
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+
+    return render_template("home.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
