bc929fe04935013a2d65e2c7fe7ee546c9f0c06b
zezs <my.version.ctrl@gmail.com>
2024-05-04 23:19:36 +0200

    sending messages

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, request, session, url_for, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "Qwerty16"
+app.config["SECRET_KEY"] = "dsasdf"
 socketio = SocketIO(app)
 
 rooms = {}
-
 
 def generate_unique_code(length):
     """
@@ -21,12 +20,11 @@ def generate_unique_code(length):
         code = ""
         for i in range(length):
             code += random.choice(ascii_uppercase)
-
+        
         if code not in rooms:
             break
     
     return code
-
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -36,43 +34,54 @@ def home():
         code = request.form.get("code")
         join = request.form.get("join", False)
         create = request.form.get("create", False)
-    
+
         if not name:
-                return render_template("home.html", error="Please enter name!", code=code, name=name)
-            
+            return render_template("home.html", error="Please enter a name.", code=code, name=name)
+
         if join != False and not code:
-            return render_template("home.html", error="Please enter a room code!", code=code, name=name)
-            
+            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
+        
         room = code
         if create != False:
             # creating a room
             room = generate_unique_code(4)
-            rooms[room] = {"members":0, "messages": []}
+            rooms[room] = {"members": 0, "messages": []}
         elif code not in rooms:
-            # if not creating a room then joining a room(wrong code)
-            return render_template("home.html", error="Room does not exist!", code=code, name=name)
+            #if not creating a room then joing a room(wrong code)
+            return render_template("home.html", error="Room does not exist.", code=code, name=name)
         
         session["room"] = room
         session["name"] = name
-
+        
         # if not creating a room and code is correct
         return redirect(url_for("room"))
-    
+
     return render_template("home.html")
-
-
 
 @app.route("/room")
 def room():
     room = session.get("room")
     if room is None or session.get("name") is None or room not in rooms:
         return redirect(url_for("home"))
+
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return 
     
-    return render_template("room.html")
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
 
 @socketio.on("connect")
-def connect(auth): 
-    """ User joining th room"""
+def connect(auth):
     room = session.get("room")
     name = session.get("name")
     if not room or not name:
@@ -83,25 +92,22 @@ def connect(auth):
     
     join_room(room)
     send({"name": name, "message": "has entered the room"}, to=room)
-    rooms[room]["members"] +=1
+    rooms[room]["members"] += 1
     print(f"{name} joined room {room}")
-
 
 @socketio.on("disconnect")
 def disconnect():
-    """User leaving the room"""
     room = session.get("room")
     name = session.get("name")
     leave_room(room)
 
     if room in rooms:
         rooms[room]["members"] -= 1
-        if rooms[room]["members"] <=0:
+        if rooms[room]["members"] <= 0:
             del rooms[room]
-
+    
     send({"name": name, "message": "has left the room"}, to=room)
     print(f"{name} has left the room {room}")
-    
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
