c09ce4abb6a3260655f49d84953da44a5b3772f2
zezs <my.version.ctrl@gmail.com>
2024-05-04 22:32:35 +0200

    socket connection implemented

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def home():
         if create != False:
             # creating a room
             room = generate_unique_code(4)
-            rooms[room] = {"memebrs":0, "messages": []}
+            rooms[room] = {"members":0, "messages": []}
         elif code not in rooms:
             # if not creating a room then joining a room(wrong code)
             return render_template("home.html", error="Room does not exist!", code=code, name=name)
@@ -70,7 +70,38 @@ def room():
     
     return render_template("room.html")
 
+@socketio.on("connect")
+def connect(auth): 
+    """ User joining th room"""
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] +=1
+    print(f"{name} joined room {room}")
 
+
+@socketio.on("disconnect")
+def disconnect():
+    """User leaving the room"""
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <=0:
+            del rooms[room]
+
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
+    
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
